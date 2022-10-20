0291bb93673ac1a943f595f904cec1dd47cdb408
龙也 <yanchi.zyc@alibaba-inc.com>
2022-10-21 00:32:11 +0800

    🎉 Begin a project.

---
+++ src/pages/tutorial/basic/index.tsx
@@ -0,0 +1,10 @@
+import { PageContainer } from '@ant-design/pro-layout';
+import ProCard from '@ant-design/pro-card';
+
+export default () => {
+  return (
+    <PageContainer>
+      <ProCard ghost>content</ProCard>
+    </PageContainer>
+  );
+};
